32e0c00120f32292fc748e7f64496ba8d8f6e5ba
Ana_invernizzi <anadiangelo20@gmail.com>
2026-04-26 17:31:57 -0300

    Adicionando novos metodos para mover a nave do player para a direita e esquerda utilizando 'a' e 'd'

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main():
         screen.fill("black")
 
         player.draw(screen)
-
+        player.update(dt)
         pygame.display.flip()
 
         # Chamando o método que limita o FPS para 60
