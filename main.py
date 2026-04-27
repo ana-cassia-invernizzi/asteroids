507083f3a4bfebf079ef36873ab2a9dc3fc9927f
Ana_invernizzi <anadiangelo20@gmail.com>
2026-04-27 20:52:09 -0300

    Criando o metodo de colisao, para calcular quando o jogador colide com o asteroide e dando game over fechando o jogo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -47,6 +48,12 @@ def main():
         updatable.update(dt)
         for obj in drawable:
             obj.draw(screen)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         pygame.display.flip()
 
